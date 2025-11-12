daacc9e7211491fbebc2cf3e5ab8b67e95851ec1
killah2008 <fernandessoh@gmail.com>
2025-11-12 15:18:28 +0000

    recupération de la version de base

--- src/main/java/ci/dgmp/sigefbackend/SigefBackendApplication.java
+++ src/main/java/ci/dgmp/sigefbackend/SigefBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SigefBackendApplication {
+public class SigefBackendApplication {//
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigefBackendApplication.class, args);
